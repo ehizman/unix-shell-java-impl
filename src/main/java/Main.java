24e8e3d30e4827b5058d80a7fb8227489f320d7d
ehizman <edemaehiz@gmail.com>
2025-03-29 02:39:24 +0100

    implementing REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
@@ -8,6 +10,9 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        if (input.equalsIgnoreCase("exit")){
+            exit(0);
+        }
         System.out.println(input + ": command not found");
         main(null);
     }
