4e4878ac1adb95ff3c9252e3753d989ac7f70d5c
ehizman <edemaehiz@gmail.com>
2025-03-29 18:16:03 +0100

    'implement type builtin command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,30 @@
 import java.util.Scanner;
 
-import static java.lang.System.exit;
-import static java.lang.System.out;
+import static java.lang.System.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
 
         System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        if (input.startsWith("exit")){
-            return;
-        }
-        if (input.startsWith("echo")) {
-            System.out.println(input.substring(5));
-        } else {
+
+        while (!input.startsWith("exit")){
+            if (input.startsWith("type")) {
+                String command = input.substring(5);
+                if (command.startsWith("exit") || command.startsWith("echo")) {
+                    out.println(command +" is a shell builtin");
+                } else {
+                    out.println(command + ": not found");
+                }
+            }
+            if (input.startsWith("echo")) {
+                System.out.println(input.substring(5));
+            }
             out.println(input + ": command not found");
+            System.out.print("$ ");
+            input = scanner.nextLine();
         }
-        main(null);
     }
 }
